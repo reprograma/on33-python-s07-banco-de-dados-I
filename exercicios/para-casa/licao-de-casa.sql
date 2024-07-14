393a7471d78f74ec344c0fd1c92712425fcafef8
Alexciane Lima <alexcianeal@gmail.com>
2024-07-14 13:45:01 -0300

    subindo_casa07

--- exercicios/para-casa/licao-de-casa.sql
+++ exercicios/para-casa/licao-de-casa.sql
@@ -1,15 +1,20 @@
 -- create table estoque
 
-
-
-
-
 -- Obter a quantidade disponível em estoque para um determinado livro.
-
-
-
 
 -- Adicionar unidades ao estoque de um livro específico.
 
+CREATE TABLE estoque (
+    id INTEGER PRIMARY KEY,
+  	livros_id INTEGER,
+  	quantidade INTEGER,
+  	FOREIGN KEY (livros_id) REFERENCES (livros_id)
+);
 
+INSERT INTO estoque (livros_id, quantidade) VALUES (1, 5);
+INSERT INTO estoque (livros_id, quantidade) VALUES (2, 10);
+INSERT INTO estoque (livros_id, quantidade) VALUES (3, 12);
+
+SELECT * from estoque
+WHERE livros_id = 2
 
