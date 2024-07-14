d52a7a5fd5c91c6d23076ccb9707d823136b0b93
Ladiane P.S <aladydyljc@gmail.com>
2024-07-13 21:51:35 -0300

    atividade seamana 07 Ladiane

--- exercicios/para-casa/licao-de-casa.sql
+++ exercicios/para-casa/licao-de-casa.sql
@@ -1,15 +1,36 @@
 -- create table estoque
 
+CREATE table estoque (
+  id INTEGER PRIMARY KEY,
+  livro_id INTEGER,
+  quantidade INTEGER,
+  FOREIGN KEY (livro_id) REFERENCES livros(id)
+  );
 
+INSERT into estoque (livro_id, quantidade) VALUES (1, 2);
+INSERT INTO estoque (livro_id, quantidade) VALUES (1, 10); 
+INSERT INTO estoque (livro_id, quantidade) VALUES (2, 5);  
+INSERT INTO estoque (livro_id, quantidade) VALUES (3, 8);
+    
+  /*
+ Isso significa que estamos criando uma conexão entre a tabela estoque e a tabela livros. Basicamente, 
+ estamos dizendo que o valor na coluna livro_id da tabela estoque deve ser um valor que já existe na coluna id da tabela livros.
+se a tabela livros tem um livro com id 1, então a tabela estoque pode ter um registro onde livro_id é 1. 
+Se a tabela livros não tem um livro com id 1, você não pode ter livro_id 1 na tabela estoque.
+  */
 
 
 
 -- Obter a quantidade disponível em estoque para um determinado livro.
-
+SELECT quantidade 
+FROM estoque
+where livro_id = 1; -- escolhe o livro que quer nesse caso e 1
 
 
 
 -- Adicionar unidades ao estoque de um livro específico.
-
+UPDATE estoque 
+set quantidade = quantidade + 2  -- adiciona mais 2 ao estoque
+where id = 1;  -- substituir 
 
 
