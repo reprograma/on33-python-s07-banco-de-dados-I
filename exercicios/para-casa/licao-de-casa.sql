5b6fc99fd4e06a51aba25be5a74cb25eb0c47a3b
Elysa Mirela <elysacarv@gmail.com>
2024-07-16 23:13:47 -0300

    exercicio da semana 07 SQL

--- exercicios/para-casa/licao-de-casa.sql
+++ exercicios/para-casa/licao-de-casa.sql
@@ -1,15 +1,41 @@
 -- create table estoque
+CREATE TABLE estoque (
+    id INTEGER PRIMARY KEY,
+    livro_id INT,
+    quantidade INT
+);
 
+INSERT INTO estoque (id, livro_id, quantidade)
+VALUES
+    (1, 1, 20);
+
+INSERT INTO estoque (id, livro_id, quantidade)
+VALUES
+    (2, 2, 10);
+    
+INSERT INTO estoque (id, livro_id, quantidade)
+VALUES
+    (3, 3, 15);  
 
 
 
 
 -- Obter a quantidade disponível em estoque para um determinado livro.
 
+SELECT quantidade
+FROM estoque
+WHERE livro_id = 2; 
+
+
 
 
 
 -- Adicionar unidades ao estoque de um livro específico.
+UPDATE estoque
+SET quantidade = 4
+WHERE livro_id = 2;
 
+SELECT *
+FROM estoque;
 
 
